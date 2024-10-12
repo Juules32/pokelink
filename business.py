89a3f8ec3eef3ce05d1c5f5243f4df17bde24815
juules32 <56916699+Juules32@users.noreply.github.com>
2024-10-12 15:15:17 +0200

    separated concerns and added endpoints

---
+++ business.py
@@ -0,0 +1,37 @@
+from typing import Union
+from networkx import Graph
+from db import Database
+from model import AdjacencyData, PokemonData, Puzzle
+
+class Business:
+    def __init__(self):
+        self.db = Database()
+        self.graph = self.db.get_graph()
+    
+    def get_graph(self) -> Graph:
+        return self.graph
+    
+    def get_puzzle(self, date: Union[str, None] = None) -> Puzzle:
+        return self.db.get_puzzle(date)
+    
+    def get_adjacency_data(self, guess: str) -> AdjacencyData:
+        guess_graph_data = self.graph.nodes[guess]
+        guess_data = PokemonData(
+            name=guess,
+            types=guess_graph_data["types"],
+            region=guess_graph_data["region"]
+        )
+
+        neighbor_data: list[PokemonData] = []
+        for neighbor in self.graph.neighbors(guess):
+            neighbor_graph_data = self.graph.nodes[neighbor]
+            neighbor_data.append(PokemonData(
+                name=neighbor,
+                types=neighbor_graph_data["types"],
+                region=neighbor_graph_data["region"]
+            ))
+
+        return AdjacencyData(
+            guess=guess_data,
+            adjacent_pokemon=neighbor_data
+        )
--- model.py
+++ model.py
@@ -1,7 +1,19 @@
 from pydantic import BaseModel
 
+# Stored in the database as daily puzzles
 class Puzzle(BaseModel):
-    source: str
-    target: str
-    shortest_path: list[str]
-    shortest_path_length: int
+    source: str                             # The name of the starting pokémon
+    target: str                             # The name of the finishing pokémon
+    shortest_path: list[str]                # The names of the pokémon of the shortest path
+    shortest_path_length: int               # The length of the shortest path
+
+# A grouping of pokémon data used in AdjacencyData
+class PokemonData(BaseModel):
+    name: str                               # The name of the pokémon
+    types: list[str]                        # The types of the pokémon
+    region: str                             # The region of the pokémon
+
+# Returned as response to valid client guess
+class AdjacencyData(BaseModel):
+    guess: PokemonData
+    adjacent_pokemon: list[PokemonData]
