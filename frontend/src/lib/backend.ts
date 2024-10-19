1d104a0da90696fa0b7a3e903db5a0acc742c8a2
Benjamin Jensen <Benjaminbyvej@gmail.com>
2024-10-19 21:17:33 +0200

    Prettier and cleaner frontend

--- frontend/src/lib/backend.ts
+++ frontend/src/lib/backend.ts
@@ -1,14 +1,12 @@
-import type { Node, Puzzle } from "$lib/interfaces";
+import type { AdjacencyData, Puzzle } from "$lib/interfaces";
 
 // Function to fetch the list of Pokémon and filter based on search query
-export async function fetchAdjacencyData(guess: string): Promise<Node> {
+export async function fetchAdjacencyData(guess: string): Promise<AdjacencyData> {
 	const response = await fetch(`http://localhost/adjacency_data/${guess}`)
     const data = await response.json()
-    const result: Node = {
-        name: data.guess.name,
-        id: data.guess.id,
-        types: data.guess.types,
-        region: data.guess.region
+    const result: AdjacencyData = {
+        guess: data.guess,
+        adjacentPokemon: data.adjacent_pokemon
     }
     return result
 }
@@ -16,7 +14,11 @@ export async function fetchAdjacencyData(guess: string): Promise<Node> {
 export async function fetchPuzzle(): Promise<Puzzle> {
     const response = await fetch(`http://localhost/puzzle`)
     const data = await response.json()
-    console.log(data)
-    const result: Puzzle = data
+    const result: Puzzle = {
+        source: data.source,
+        target: data.target,
+        shortestPath: data.shortest_path,
+        shortestPathLength: data.shortest_path_length
+    }
     return result
 }
--- frontend/src/lib/state.ts
+++ frontend/src/lib/state.ts
@@ -1,11 +1,39 @@
-import type { Node } from "$lib/interfaces";
+import type { AdjacencyData, Node, Puzzle } from "$lib/interfaces";
 import { fetchAdjacencyData } from "$lib/backend";
-import { writable } from 'svelte/store';
+import { get, writable } from 'svelte/store';
+
+export const puzzle = writable<Puzzle>();
 
 export const guessedNodes = writable<Node[]>([]);
 
-export async function addGuessNode(guess: string) {
-    const newNode: Node = await fetchAdjacencyData(guess)
-    guessedNodes.update((nodes) => [...nodes, newNode]);
-    console.log(guessedNodes)
+export const validGuesses = writable<Node[]>([]);
+
+// Adding local node data means the frontend can render before requesting
+// new adjacency data from the backend, resulting in snappy updates.
+function addLocalNode(guess: string) : boolean {
+    const newNode = get(validGuesses).find((node) => node.name == guess)
+    if (newNode) {
+        guessedNodes.update((nodes) => [...nodes, newNode]);
+        console.log("Loaded node data locally")
+        return false
+    }
+    else {
+        console.log("Failed to load data locally")
+        return true
+    }
+}
+
+export async function updateState(guess: string) {
+    console.log(`Updating state for guess: ${guess}`)
+    const failedLocal = addLocalNode(guess)
+    const adjacencyData: AdjacencyData = await fetchAdjacencyData(guess)
+    if (failedLocal) {
+        guessedNodes.update((nodes) => [...nodes, adjacencyData.guess]);
+        console.log("Loaded node data from backend instead")
+    }
+    validGuesses.set(adjacencyData.adjacentPokemon);
+
+    if (get(puzzle).target == guess) {
+        console.log("You win!")
+    }
 }
