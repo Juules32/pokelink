06bdf625f8f4192835a2245d119c14ae74a4c382
juules32 <56916699+Juules32@users.noreply.github.com>
2024-10-21 00:14:50 +0200

    prefetching and moved search logic into search component

--- frontend/src/lib/pokeAPI.ts
+++ frontend/src/lib/pokeAPI.ts
@@ -1,49 +1,15 @@
 import { isNameLegal } from '$lib/util'
 import type { SearchResult } from '$lib/interfaces'
+import { get, writable } from 'svelte/store';
 
 const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0")
 const data = await response.json()
-const searchResults: SearchResult[] = data.results
-
-// Filter and return search results based on search query
-export async function fetchSearchData(search: string): Promise<SearchResult[]> {
-	try {
-		const searchLowerCase = search.toLowerCase()
-		const filteredResponse = searchResults
-			.filter((pokemon: SearchResult) => isNameLegal(pokemon.name))
-			.filter((pokemon: SearchResult) => pokemon.name.includes(searchLowerCase))
-			.sort((a: SearchResult, b: SearchResult) => customSort(a, b, searchLowerCase))
-			.slice(0, 10)
-
-		const spriteUrls: SearchResult[] = await Promise.all(
-			filteredResponse.map(async (pokemon: SearchResult) => {
-				const spriteUrl = await getPokemonSprite(pokemon.url)
-				return { name: pokemon.name, url: spriteUrl }
-			})
-		)
-		return spriteUrls
-	} catch (error) {
-		console.error(error)
-		return []
-	}
-}
-
-// Helper function for custom sorting
-function customSort(a: SearchResult, b: SearchResult, searchLowerCase: string): number {
-	const startsWithSearchStringA = a.name.toLowerCase().startsWith(searchLowerCase)
-	const startsWithSearchStringB = b.name.toLowerCase().startsWith(searchLowerCase)
-
-	if (startsWithSearchStringA && !startsWithSearchStringB) {
-		return -1
-	} else if (!startsWithSearchStringA && startsWithSearchStringB) {
-		return 1
-	} else {
-		return a.name.localeCompare(b.name)
-	}
-}
+const pokemonResults: SearchResult[] = data.results
+export const validResults: SearchResult[] = pokemonResults.filter((pokemon) => isNameLegal(pokemon.name));
+export const localUrls = writable<{ [key: string]: string }>({});
 
 // Fetch the Pokémon sprite from a given URL
-async function getPokemonSprite(url: string) : Promise<string> {
+export async function getPokemonSprite(url: string) : Promise<string> {
 	try {
 		const response = await fetch(url)
 		const data = await response.json()
@@ -51,5 +17,27 @@ async function getPokemonSprite(url: string) : Promise<string> {
 	} catch (error) {
 		console.error(error)
 		return ""
+	}
+}
+
+// Fetches and stores name to url data locally
+export async function prefetchSprites() {
+	// Set local url data if it already exists
+	const localUrlData = localStorage.getItem("spriteUrls")
+	if (localUrlData) {
+		localUrls.set(JSON.parse(localUrlData))
+	}
+
+	// Otherwise, fetch sprite urls and set them
+	else {
+		await Promise.all(
+			validResults.map(async (pokemon: SearchResult) => {
+				const spriteUrl = await getPokemonSprite(pokemon.url)
+				get(localUrls)[pokemon.name] = spriteUrl;
+			})
+		)
+
+		// Caches urls
+		localStorage.setItem("spriteUrls", JSON.stringify(get(localUrls)))
 	}
 }
