01f0ae1863bf6cf051d9a11a618c9f712577c143
Benjamin Jensen <Benjaminbyvej@gmail.com>
2024-10-20 14:46:37 +0200

    type validation for endpoints

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
+from model import AdjacencyData, PokemonNode, Puzzle
 from business import Business
 from fastapi.middleware.cors import CORSMiddleware
 
@@ -24,13 +25,17 @@ def get_test():
     return bn.get_graph()
 
 @app.get("/puzzle")
-def get_puzzle():
+def get_puzzle() -> Puzzle:
     return bn.get_puzzle()
 
 @app.get("/puzzle/{date}")
-def get_puzzle(date: str):
+def get_puzzle(date: str) -> Puzzle:
     return bn.get_puzzle(date)
 
 @app.get("/adjacency_data/{guess}")
-def get_adjacency_data(guess: str):
+def get_adjacency_data(guess: str) -> AdjacencyData:
     return bn.get_adjacency_data(guess)
+
+@app.get("/hint")
+def get_hint(source: str, target: str) -> PokemonNode:
+    return bn.get_hint(source, target)
