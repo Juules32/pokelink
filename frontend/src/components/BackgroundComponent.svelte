6e221f433e74f9cf1c69c1b092f6f7de44ca5878
juules32 <56916699+Juules32@users.noreply.github.com>
2024-10-26 14:19:26 +0200

    small improvements

--- frontend/src/components/BackgroundComponent.svelte
+++ frontend/src/components/BackgroundComponent.svelte
@@ -1,5 +1,27 @@
 <script lang="ts">
-    import { getRandomBackgroundPath } from "$lib/util";
+    import { base } from "$app/paths";
+    
+    function getRandomBackgroundPath() {
+        const backgroundNames: string[] = [
+            "forest",
+            "berry-forest",
+            "cave",
+            "cave2",
+            "cave3",
+            "cave4",
+            "chamber",
+            "hideout",
+            "mansion",
+            "mt-ember",
+            "power-plant",
+            "safari",
+            "tower",
+            "tower2",
+            "victory-road",
+            "warehouse",
+        ];
+        return `${base}/backgrounds/${backgroundNames[Math.floor(Math.random() * backgroundNames.length)]}.png`;
+    }
 </script>
 
 <div
