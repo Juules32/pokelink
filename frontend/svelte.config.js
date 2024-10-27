1cac56c375ac902f2db41af4a2f06a4fccfe6201
juules32 <56916699+Juules32@users.noreply.github.com>
2024-10-27 21:37:50 +0100

    changed to adapter-vercel

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -14,11 +14,6 @@ const config = {
 		}),
 		paths: {
 			base: process.argv.includes('dev') ? '' : (process.env.BASE_PATH ?? '')
-		},
-		prerender: {
-			handleHttpError: () => {
-				return;
-			}
 		}
 	}
 };
